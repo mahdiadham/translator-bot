0bdb3572c3bad1c49ff3a5633b21cb9a65e5d4f3
Mahdi Adham <Cicada.3301.M.A.110@gmail.com>
2026-06-22 12:16:56 +0330

    update database configs

--- configs/database.js
+++ configs/database.js
@@ -1,7 +1,8 @@
 import mysql from "mysql2";
 
 const databaseConfig = {
-    host:process.env.DB_URL,
+    host:process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user:process.env.DB_USERNAME,
     password:process.env.DB_PASSWORD,
     database:process.env.DB_NAME,
